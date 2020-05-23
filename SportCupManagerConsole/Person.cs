ee49af41f14854683c17498de449a4452c6c4906
Krystian Szmigielski <krystian.szmigielski2@gmail.com>
2020-05-23 14:35:43 +0200

    dodgeball changes

---
+++ SportCupManagerConsole/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportCupManagerConsole
+{
+    abstract class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected Byte age;
+
+        public Person(string firstName, string lastName, Byte age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+}
---
+++ SportCupManagerConsole/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportCupManagerConsole
+{
+    class Player : Person
+    {
+        private Byte number;
+
+        public Player(string firstName, string lastName, Byte age, Byte number) : base(firstName, lastName, age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public void setNumber(Byte number)
+        {
+            this.number = number;
+        }
+
+        public Byte getNumber()
+        {
+            return number;
+        }
+    }
+}
