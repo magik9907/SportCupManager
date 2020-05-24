c161d60c61a2a5c344fcb4237677eb8ce9ac01dd
magik99 <lukaszmilewski99@gmail.com>
2020-05-24 13:38:48 +0200

    implement interface

--- SportCupManagerConsole/Torunament.cs
+++ SportCupManagerConsole/Torunament.cs
@@ -6,7 +6,12 @@ namespace TournamentManager
 {
     interface ITournament
     {
-    
+    + setLeague()
++ setPlayOff(teams:List<ITeam>)
++ addReferee(r:Referee)
++ removeReferee(r: Referee)
++ addTeam(t:ITeam)
++ removeTeam(t:I Team)
     }
 
     class Tournament : ITournament
