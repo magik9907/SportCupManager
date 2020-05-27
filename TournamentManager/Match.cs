853c1271b1f4871d19f48cb74f8045a34dd7addf
Kamil Milewski <kamilewski@yahoo.com>
2020-05-27 19:42:55 +0200

    Finished TugOfWarMatch and DodgeballMatch

--- TournamentManager/Match.cs
+++ TournamentManager/Match.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Runtime.CompilerServices;
 
 namespace TournamentManager
 {
-	//exceptions used by class are defined below the class
+	//exceptions used by class are defined below the class (will be changed in the future)
 	public abstract class Match
 	{
 		private ITeam teamA;
@@ -13,26 +13,27 @@ namespace TournamentManager
 		private ITeam winner;
 		private Referee refA;
 		//ref is a reserved word, so I changed it to ref_ 
-		public Match(ITeam a, ITeam b, Referee ref_)
+		public Match(ITeam a, ITeam b, List<Referee> ref_)
 		{
 			teamA = a;
 			teamB = b;
-			refA = ref_;
+			refA = ref_.ElementAt(0);
 		}
 		//Function takes a list of referees because VolleyballMatch needs 3 of them
-		private virtual void setReferees(List<Referee> ref_) { refA = ref_.ElementAt(0); }
+		public virtual void setReferees(List<Referee> ref_) { refA = ref_.ElementAt(0); }
 		public void setWinner(ITeam winner_) { winner = winner_; }
-		private string getWinner() { return winner; }
+		public string getWinner() { return winner.ToString(); }
 		//those virtual methods will be defined in subclasses
 		public virtual void setStat(string stat) { }
 		public virtual string getStat() { }
+		//It's just a basic try, can be changed if needed
 	}
 
 	class TugOfWarMatch : Match
 	{
 		private float matchLength;
 		//constructor uses a constructor of its superclass
-		public TugOfWarMatch(Team a, Team b, Referee ref_) : base(a, b, ref_) { }
+		public TugOfWarMatch(Team a, Team b, List<Referee> ref_) : base(a, b, ref_) { }
 		//This is based on the assumption that stat is going to be in seconds (possibly with miliseconds)
 		public override void setStat(string stat)
 		{
@@ -40,6 +41,8 @@ namespace TournamentManager
 			try
 			{
 				matchLength = float.Parse(stat, Globalization.CultureInfo.InvariantCulture);
+				if (matchLength < 0)
+					throw NegativeMatchLengthException;
 			}
 			//float.parse throws FormatException if stat can't be converted
 			catch (FormatException e)
@@ -54,7 +57,8 @@ namespace TournamentManager
 		}
 	}
 
-	class NotNumberMatchLengthException : Exception
+	//Exception if match length is not a number
+	public class NotNumberMatchLengthException : Exception
 	{
 		public override string Message
 		{
@@ -65,23 +69,48 @@ namespace TournamentManager
 		}
 	}
 
+	//Exception if match length is negative
+	public class NegativeMatchLengthException : Exception
+    {
+        public override string Message
+        {
+			get
+            {
+				return "Match length is supposed to not be negative!";
+            }
+        }
+    }
+
 	class DodgeballMatch : Match
 	{
 		//we might need to change that name
 		private int winnerPlayersLeft;
-		public DodgeballMatch(Team a, Team b, Referee ref_) : base(a, b, ref_) { }
+		public DodgeballMatch(Team a, Team b, List<Referee> ref_) : base(a, b, ref_) { }
 		public override void setStat(string stat)
 		{
+			//if stat is not a number parse will throw format exception
 			try
 			{
 				winnerPlayersLeft = int.Pasre(stat);
+				if(winnerPlayersLeft <= 0)
+                {
+					throw NegativePlayersNumberException;
+                }
+				if(winnerPlayersLeft > 6)
+                {
+					throw TooHighPlayersLeftException;
+                }
 			}
 			catch (FormatException q)
 			{
 				throw NotIntPlayersException;
 			}
 		}
-	}
+        public override string getStat()
+        {
+			return winnerPlayersLeft.ToString();
+        }
+    }
 
 	//exception if number of players was not a whole number
 	public class NotIntPlayersException : Exception
@@ -94,4 +123,51 @@ namespace TournamentManager
 			}
 		}
 	}
+
+	//exception if number of players was negative
+	public class NegativePlayersNumberException : Exception
+    {
+        public override string Message
+        {
+            get
+			{
+				return "Number of players should be positive!";
+			}
+        }
+    }
+
+	//Exception if number of players left was higher than players allowed on the field
+	public class TooHighPlayersLeftException : Exception
+    {
+		public override string Message
+        {
+			get
+            {
+				return "Team can't have more players left than it started the game with (6)";
+            }
+        }
+    }
+
+	public class VolleyballMatch : Match
+    {
+		private List<Referee> assistantReferees = new List<Referee>(2);
+		//the score means points gained by team in each set
+		//score from x set is kept under x-1 index in the table
+		private int[] scoreTeamA;
+		private int[] scoreTeamB;
+        public VolleyballMatch(Team a, Team b, List<Referee> ref_) : base(a, b, ref_)
+        {
+			setReferees(ref_);
+        }
+		public override void setReferees(List<Referee> ref_)
+        {
+            base.setReferees(ref_);
+			assistantReferees.AddRange = ref_.GetRange(1, 2);
+        }
+		//the expected format is "a: scoreInSet1, scoreInSet2, scoreInSet3(0 if not played). b: scoreInSet1, scoreInSet2, scoreInSet3(0 if not played)"
+        public override void setStat(string stat)
+        {
+			stat.Split(new char[] {':', ',' });
+        }
+    }
 }
