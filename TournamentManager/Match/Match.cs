1edd4c57149fdf89b665f34a7382480373f48472
Kamil Milewski <kamilewski@yahoo.com>
2020-06-05 13:10:18 +0200

    VolleyballMatch in progess

--- TournamentManager/Match/Match.cs
+++ TournamentManager/Match/Match.cs
@@ -14,31 +14,49 @@ namespace TournamentManager
 			private TTeam.ITeam teamB;
 			private TTeam.ITeam winner;
 			private TPerson.Referee refA;
-			//ref is a reserved word, so I changed it to ref_ 
-			public Match(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> ref_)
+			public Match(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> @ref)
 			{
 				teamA = a;
 				teamB = b;
-				refA = ref_.ElementAt(0);
+				refA = @ref.ElementAt(0);
 			}
 			//Function takes a list of referees because VolleyballMatch needs 3 of them
 			public virtual void setReferees(List<TPerson.Referee> ref_) { refA = ref_.ElementAt(0); }
 			public void setWinner(TTeam.ITeam winner_) { winner = winner_; }
 			public string getWinner() { return winner.ToString(); }
 			//those virtual methods will be defined in subclasses
-			public virtual void setStat(string stat) { }
+			public virtual void setResult(string stat, TTeam.ITeam @winner)
+			{
+				if (@winner == teamA || winner == teamB)
+					winner = winner;
+				else
+					throw new TeamIsNotPlayingException();
+			}
 			public virtual string getStat() { return null; }
 			//It's just a basic try, can be changed if needed
 		}
+
+		//Exception if team set as winner is not playing in the match
+		public class TeamIsNotPlayingException : Exception
+        {
+            public override string Message
+            {
+                get 
+				{
+					return "Team you want to set as a winner is not playing!";
+				}
+            }
+        }
 
 		public class TugOfWarMatch : Match
 		{
 			private float matchLength;
 			//constructor uses a constructor of its superclass
-			public TugOfWarMatch(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> ref_) : base(a, b, ref_) { }
+			public TugOfWarMatch(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> @ref) : base(a, b, @ref) { }
 			//This is based on the assumption that stat is going to be in seconds (possibly with miliseconds)
-			public override void setStat(string stat)
+			public override void setResult(string stat, TTeam.ITeam @winner)
 			{
+				base.setResult(stat, @winner);
 				//a safety check just in case stat is not a number 
 				try
 				{
@@ -87,9 +105,10 @@ namespace TournamentManager
 		{
 			//we might need to change that name
 			private int winnerPlayersLeft;
-			public DodgeballMatch(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> ref_) : base(a, b, ref_) { }
-			public override void setStat(string stat)
+			public DodgeballMatch(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> @ref) : base(a, b, @ref) { }
+			public override void setResult(string stat, TTeam.ITeam @winner)
 			{
+				base.setResult(stat, @winner);
 				//if stat is not a number parse will throw format exception
 				try
 				{
@@ -155,21 +174,60 @@ namespace TournamentManager
 			private List<TPerson.Referee> assistantReferees = new List<TPerson.Referee>(2);
 			//the score means points gained by team in each set
 			//score from x set is kept under x-1 index in the table
-			private int[] scoreTeamA;
-			private int[] scoreTeamB;
-			public VolleyballMatch(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> ref_) : base(a, b, ref_)
+			private int[] scoreTeamA = new int[3];
+			private int[] scoreTeamB = new int[3];
+			public VolleyballMatch(TTeam.ITeam a, TTeam.ITeam b, List<TPerson.Referee> @ref) : base(a, b, @ref)
 			{
-				setReferees(ref_);
+				setReferees(@ref);
 			}
-			public override void setReferees(List<TPerson.Referee> ref_)
+			public override void setReferees(List<TPerson.Referee> @ref)
 			{
-				base.setReferees(ref_);
-				assistantReferees.AddRange( ref_.GetRange(1, 2));
+				base.setReferees(@ref);
+				assistantReferees.AddRange( @ref.GetRange(1, 2));
 			}
-			//the expected format is "a: scoreInSet1, scoreInSet2, scoreInSet3(0 if not played). b: scoreInSet1, scoreInSet2, scoreInSet3(0 if not played)"
-			public override void setStat(string stat)
+			//the expected format is "a: scoreInSet1, scoreInSet2, scoreInSet3(0 if not played). b:scoreInSet1, scoreInSet2, scoreInSet3(0 if not played)"
+			public override void setResult(string stat, TTeam.ITeam @winner)
 			{
-				stat.Split(new char[] {':', ',' });
+				base.setResult(stat, @winner);
+				//split the strings into strings containing name of the teams and their scores
+				string[] tmp = stat.Split(new string[] {". ", ", ", ": "});
+				//string should split into 8 smaller string (2 for names of teams, 6 in total for scores in sets)
+				if (tmp.Length != 8)
+					throw new WrongStatFormatException();
+				for(int i = 0; i < 3; i++)
+                {
+					scoreTeamA[i] = int.Parse(tmp[i + 1]);
+					scoreTeamB[i] = int.Parse(tmp[i + 5]);
+                }
+			}
+		}
+
+		//Exception if string stat got seprated into a different amount of strings than expected
+		public class WrongStatFormatException : Exception
+		{
+			public override string Message
+			{
+				get
+				{
+					return "Format of string \"stat\" is incorrect!";
+				}
+			}
+		}
+
+		//Exception if the team that was set as winner lost based on the points from stats
+		public class WrongWinnerException : Exception
+		{
+			public TTeam.ITeam supposedWinner;
+			public WrongWinnerException(TTeam.ITeam winner)
+            {
+				supposedWinner = winner;
+            }
+			public override string Message
+			{
+				get
+				{
+					return "Team" + supposedWinner.ToString() + "lost despite winning 2 sets!";
+				}
 			}
 		}
 	}
