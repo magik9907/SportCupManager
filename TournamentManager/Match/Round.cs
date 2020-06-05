183e5c8074e03e461bbfc19f9d057873d3d5391e
Kamil Milewski <kamilewski@yahoo.com>
2020-06-05 19:04:34 +0200

    Finished Round

--- TournamentManager/Match/Round.cs
+++ TournamentManager/Match/Round.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using TournamentManager.TPerson;
+using TournamentManager.TTeam;
 
 namespace TournamentManager
 {
@@ -8,7 +11,7 @@ namespace TournamentManager
     {
         public class PlayOff
         {
-            public PlayOff(List<TTeam.ITeam> t, List<TPerson.Referee> r)
+            public PlayOff(List<TTeam.ITeam> t, List<TPerson.Referee> @ref)
             {
 
             }
@@ -16,7 +19,7 @@ namespace TournamentManager
 
         public class League
         {
-            public League(List<TTeam.ITeam> t, List<TPerson.Referee> r)
+            public League(List<TTeam.ITeam> t, List<TPerson.Referee> @ref)
             {
 
             }
@@ -24,6 +27,100 @@ namespace TournamentManager
 
         public class Round
         {
+            private List<TMatch.Match> listMatches = new List<TMatch.Match>();
+            private int[] date = new int[3];
+            private string roundName;
+            public Round(string name, int[] date)
+            {
+                int maxDays;
+                if (date.Length != 3)
+                    throw new WrongDateFormatException();
+                if(date[1] > 12 || date[1] <= 0)
+                    throw new WrongMonthException();
+                if (date[1] == 2)
+                    if ((date[2] % 4 == 0 && date[2] % 100 != 0) || date[2] % 400 == 0)
+                        maxDays = 29;
+                    else
+                        maxDays = 28;
+                else
+                    if ((date[1] < 8 && date[1] % 2 == 1) || (date[1] >= 8 && date[1] % 2 == 0))
+                        maxDays = 31;
+                    else
+                        maxDays = 30;
+                if (date[0] > maxDays || date[0] <= 0)
+                    throw new WrongDayException();
+                this.date = date;
+                roundName = name;
+            }
+
+            public string getRoundName()
+            {
+                return roundName;
+            }
+
+            public void addMatch(TMatch.Match match, List<Referee> r)
+            {
+                match.setReferees(r);
+                for(int i = 0; i < listMatches.Count; i++)
+
+                listMatches.Add(match);
+            }
+        }
+
+        public class AlreadyPlayingException : Exception
+        {
+            private TMatch.Match match;
+            private TTeam.ITeam team;
+            public AlreadyPlayingException(TMatch.Match match, TTeam.ITeam team)
+            {
+                this.match = match;
+                this.team = team;
+            }
+            public override string Message
+            {
+                get
+                {
+                    TTeam.ITeam team2;
+                    if (team == match.getTeamA())
+                        team2 = match.getTeamB();
+                    else
+                        team2 = match.getTeamA();
+                    return team + " is already scheduled to play this round against " + team2;
+                }
+            }
+        }
+
+        public class WrongDayException : Exception
+        {
+            public override string Message
+            {
+                get
+                {
+                    return "Day number is out of expected range";
+                }
+            }
+        }
+
+        public class WrongMonthException : Exception
+        {
+            public override string Message
+            {
+                get 
+                {
+                    return "Month number is out of expected range";
+                }
+            }
+        }
+
+        public class WrongDateFormatException : Exception
+        {
+            public override string Message
+            {
+                get
+                {
+                    return "Date is in the wrong format";
+                }
+            }
         }
     }
 }
