0611c5317df8f031fdf8d463b713d7198e837f5c
magik99 <lukaszmilewski99@gmail.com>
2020-06-12 15:05:36 +0200

    wpf - tournament list view

--- SportCupManager/MainWindow.xaml.cs
+++ SportCupManager/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,42 @@ namespace SportCupManager
         public MainWindow()
         {
             InitializeComponent();
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\TournamentManager\\data";
+            String[] list = Directory.GetDirectories(path);
+            List<Tournament> lists = new List<Tournament>();
+            for (int i = 0; i < list.Length; i++)
+            {
+                lists.Add(new Tournament(list[i]));
+            }
+            TournamentList.ItemsSource = lists;
+            TournamentList.Items.Refresh();
+        }
+
+
+       
+    }
+
+    public class Tournament
+    {
+        public string Name { get; }
+        public string Path { get; }
+
+        public Tournament(string path)
+        {
+            Path = path;
+            Name = getNameFromPath();
+        }
+
+        public string getNameFromPath()
+        {
+            int index = Path.LastIndexOf("\\");
+            return Path.Substring(index+1);
+        }
+
+        public override string ToString()
+        {
+            return Name.ToString();
         }
     }
 }
