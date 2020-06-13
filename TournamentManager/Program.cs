819fcb41ef7cb8dd3fb38607bf70f67b3a5a2777
Kamil Milewski <kamilewski@yahoo.com>
2020-06-13 23:55:44 +0200

    please let me pull

--- TournamentManager/Program.cs
+++ TournamentManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TournamentManager
 {
@@ -6,20 +7,37 @@ namespace TournamentManager
     {
         static void Main(string[] args)
         {
-            ITournament t = new Tournament("Name",(int)TEnum.TournamentDyscypline.dodgeball);
+            ITournament t = new Tournament("volley", (int)TEnum.TournamentDyscypline.volleyball);
             t.AddReferee(new TPerson.Referee("a", "a", 1));
             t.AddReferee(new TPerson.Referee("b", "b", 1));
             t.AddReferee(new TPerson.Referee("c", "c", 1));
             t.AddReferee(new TPerson.Referee("d", "d", 1));
             t.AddReferee(new TPerson.Referee("e", "e", 1));
-            t.AddTeam(new TTeam.DodgeballTeam("11"));
-            t.AddTeam(new TTeam.DodgeballTeam("22"));
-            t.AddTeam(new TTeam.DodgeballTeam("33"));
-            t.AddTeam(new TTeam.DodgeballTeam("44"));
-            t.AddTeam(new TTeam.DodgeballTeam("55"));
-            t.AddTeam(new TTeam.DodgeballTeam("66"));
-
+            t.AddReferee(new TPerson.Referee("f", "f", 1));
+            t.AddReferee(new TPerson.Referee("g", "g", 1));
+            t.AddReferee(new TPerson.Referee("h", "h", 1));
+            t.AddReferee(new TPerson.Referee("i", "i", 1));
+            TTeam.ITeam te1 = new TTeam.VolleyballTeam("11");
+            te1.AddPlayer(new TPerson.Player("1", "1", 1, 1));
+            te1.AddPlayer(new TPerson.Player("2", "2", 1, 1));
+            te1.AddPlayer(new TPerson.Player("3", "3", 1, 1));
+            te1.AddPlayer(new TPerson.Player("4", "4", 1, 1));
+            t.AddTeam(te1);
+            TTeam.ITeam te2 = new TTeam.VolleyballTeam("22");
+            te2.AddPlayer(new TPerson.Player("1", "1", 1, 1));
+            te2.AddPlayer(new TPerson.Player("2", "2", 1, 1));
+            te2.AddPlayer(new TPerson.Player("3", "3", 1, 1));
+            te2.AddPlayer(new TPerson.Player("4", "4", 1, 1));
+            t.AddTeam(te2);
+            t.AddTeam(new TTeam.VolleyballTeam("33"));
+            t.AddTeam(new TTeam.VolleyballTeam("44"));
+            t.AddTeam(new TTeam.VolleyballTeam("55"));
+            t.AddTeam(new TTeam.VolleyballTeam("66"));
             t.SetAutoLeague();
+            for(int i = 0; i < t.League.Teams.Count; i++)
+                for(int j = i + 1; j < t.League.Teams.Count; j++)
+                    t.League.SetResult(t.League.Teams[i], t.League.Teams[j], t.League.Teams[i].Name + ": 21, 21, 0. " + t.League.Teams[j].Name + ": 0, 0, 0");
+            
 
             Save.Tournament(t);
         }
